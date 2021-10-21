2e3c1fd3ce6ca57e6de3c9e0ccae220376494396
peejaypel <61832441+peejaypel@users.noreply.github.com>
2021-10-21 08:51:32 +0800

    Added Tomorrow app icons and changed default apk/app icon

--- app/src/androidTest/java/com/ederchua/tomorrowapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ederchua/tomorrowapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ederchua.schedulealert", appContext.packageName)
+        assertEquals("com.ederchua.TomorrowApp", appContext.packageName)
     }
 }
